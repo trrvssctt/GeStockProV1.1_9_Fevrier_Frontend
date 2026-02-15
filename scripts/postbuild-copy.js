6591bfb5416c285d1d32ca38bce4d934d86d4b4c
Seydou Dianka <diankaseydou52@gmail.com>
2026-02-15 18:10:22 +0000

    Update postbuild-copy.js

--- scripts/postbuild-copy.js
+++ scripts/postbuild-copy.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const distIndex = path.resolve(__dirname, '..', 'dist', 'index.html');
 const dist200 = path.resolve(__dirname, '..', 'dist', '200.html');
